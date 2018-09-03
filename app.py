c6410543bda362f14a57e900dd36aadcc24d0a1a
alexgQQ <alex.grand@beatport.com>
2018-09-03 14:52:27 -0600

    Create repo, firt commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from github_webhook import Webhook
+from flask import Flask
+
+app = Flask(__name__)
+webhook = Webhook(app)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@webhook.hook()
+def on_push(data):
+    print("Got push with: {0}".format(data))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
