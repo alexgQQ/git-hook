d1b9b3f87edb79e17dfe4fc9b2f8bf3ae7f5a562
alexgQQ <alex.grand@beatport.com>
2018-09-03 15:40:07 -0600

    Test webhook payload

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from github_webhook import Webhook
 from flask import Flask
+import git
+import pprint
+
+
+def update_repos(self, to_update):
+    home=r'~/Development/RPI/'
+    g = git.cmd.Git(home + to_update)
+    g.pull
 
 app = Flask(__name__)
 webhook = Webhook(app) # /postreceive endpoint
@@ -10,7 +18,7 @@ def hello_world():
 
 @webhook.hook()
 def on_push(data):
-    print("Got push with: {0}".format(data))
+    pprint.pprint(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
