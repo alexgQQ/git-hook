52f623a641270847e6030c0e121434ebfc464f41
alexgQQ <alex.grand@beatport.com>
2018-09-03 15:46:49 -0600

    Add handle for updating local repos

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from github_webhook import Webhook
 from flask import Flask
 import git
-import pprint
 
 
-def update_repos(self, to_update):
+def update_repo(to_update):
     home=r'~/Development/RPI/'
-    g = git.cmd.Git(home + to_update)
-    g.pull
+    try:
+        g = git.cmd.Git(home + to_update)
+        g.pull
+        return True
+    except:
+        print('Pull for %s repo failed !!', to_update)
+        return False
 
 app = Flask(__name__)
 webhook = Webhook(app) # /postreceive endpoint
@@ -18,7 +22,7 @@ def hello_world():
 
 @webhook.hook()
 def on_push(data):
-    pprint.pprint(data)
+    update_repo(data[u'repository'][u'name'])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
