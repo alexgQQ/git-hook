b02562a0ad27653eb89acda740dfbfe9fbf3d584
alexgQQ <alex.grand@beatport.com>
2018-09-05 22:42:19 -0600

    Add class for git init and pull

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 from github_webhook import Webhook
 from flask import Flask
-import git
+from repos import REPOS
+from git import Repo
+import sys
+import os
 
-def update_repo(to_update):
-    home = r'/home/pi/Development/RPI/'
-    try:
-        print('Pulling %s repo...'.format(to_update))
-        repo = git.Repo(home + to_update)
-        origin = repo.remotes.origin
-        origin.pull()
+
+class GitKeeper(object):
+    def __init__(self, home_path):
+        self.repos = dict()
+
+        for loc, repo_url in REPOS.items():
+            local_repo_path = home_path + loc
+
+            if not os.path.isdir(local_repo_path):
+                self.repos[loc] = Repo.clone_from(repo_url, local_repo_path)
+            else:
+                self.repos[loc] = Repo(local_repo_path)
+
+    def pull(self, repo_name):
+        repo = self.repos.get(repo_name, None)
+        if not repo:
+            print('Error - Repo: %s not found!', repo_name)
+            return False
+        repo.git.pull()
         return True
-    except Exception as e:
-        print('Pull for %s repo failed !!'.format(to_update))
-        print('Error: %s'.format(e))
-        return False
+
+
+jenkins = GitKeeper(sys.argv[1])
+
 
 app = Flask(__name__)
 webhook = Webhook(app) # /postreceive endpoint
@@ -26,7 +41,10 @@ def hello_world():
 def on_push(data):
     name = data[u'repository'][u'name']
     print('Push received for %s repo', name)
-    update_repo(name)
+    try:
+        jenkins.pull(name)
+    except Exception as e:
+        print('Push failed with %s', e)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
