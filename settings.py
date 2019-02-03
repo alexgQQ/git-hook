774299a2cdded3e7f956aa57a0889a4c068d63b7
alexgQQ <alex.grand@beatport.com>
2019-02-03 11:47:38 -0700

    Update settings

--- settings.py
+++ settings.py
@@ -1,7 +1,12 @@
+## Configuration options for the application.
 
-SECRET_KEY = b''
+# Secret Key for github verification, do not share this
+SECRET_KEY = b'12345'
 
+LOG_FILE_LOCATION = '/app/log.txt'
+
+# Dictionary mapping of repo names and github repo urls.
+# The names are used to pull the initial repos to the defined LOCAL_REPOS directory
 REPOS = {
-    'git-hook': r'https://github.com/alexgQQ/git-hook.git',
-    'rabbit': r'https://github.com/alexgQQ/rabbit.git'
+    'test-repo': r'https://github.com/alexgQQ/test-repo.git',
 }
